237d192cf0ee70b20fc37eb5e4ccfdbd97f08c63
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-04-26 12:03:44 +0200

    add trop/strat filter, detrend, started grad(sea)

--- strat_filter_on_caribic_data.py
+++ strat_filter_on_caribic_data.py
@@ -8,10 +8,14 @@ Created on Tue Apr 11 09:28:22 2023
 import numpy as np
 import sys
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn' - otherwise df[j] = val gives a warning (outliers.outliers)
 from pathlib import Path
 import datetime as dt
 
 import matplotlib.pyplot as plt
+
+import warnings # using this to suppress a gui backend userwarning, not really advisible
+warnings.filterwarnings("ignore", category=UserWarning, module='matplotlib')
 
 sys.path.insert(0, r'C:\Users\sophie_bauchinger\sophie_bauchinger\toolpac_tutorial')
 from toolpac_tutorial import Mauna_Loa, Mace_Head, Caribic, Mozart
@@ -24,7 +28,7 @@ from toolpac.age import calculate_lag as cl
 from toolpac.conv.times import datetime_to_fractionalyear, fractionalyear_to_datetime
 
 sys.path.insert(0, r'C:\Users\sophie_bauchinger\sophie_bauchinger\Caribic_data_handling')
-from C_filter import filter_strat_trop, filter_outliers
+from C_filter import filter_outliers
 import C_read
 import C_SF6_age
 import C_tools
@@ -83,14 +87,17 @@ if __name__=='__main__':
     mlo_MM.interpolate(inplace=True) # linearly interpolate missing data
     
     # loop through years of caribic data
-    for c_year in range(2016, 2020):
+    for c_year in range(2005, 2022):
         c_data = caribic_data.select_year(c_year)
-        lags = cal_time_lags(c_data, mlo_MM)
-        if all(np.isnan(np.array(lags))): 
-            print(f'no lags calculated for {c_year}'); continue
-        plot_time_lags(c_data, lags, mlo_time_lims)
+        if len(c_data[c_data['SF6 [ppt]'].notna()]) < 1: 
+            continue
+        else:
+            lags = cal_time_lags(c_data, mlo_MM)
+            if all(np.isnan(np.array(lags))): 
+                print(f'no lags calculated for {c_year}'); continue
+            plot_time_lags(c_data, lags, mlo_time_lims)
 
-#%% n2o strat trop filter
+#%% filter data into stratosphere and troposphere (using n2o as a tracer)
 def get_mlo_fit(mlo_df, substance='N2OcatsMLOm'):
     """ Given one year of reference data, find the fit parameters for n2o """
     df = mlo_df.dropna(how='any', subset=substance)
@@ -101,14 +108,14 @@ def get_mlo_fit(mlo_df, substance='N2OcatsMLOm'):
     print(f'MLO fit parameters obtained: {mlo_fit}')
     return mlo_fit
 
-
 def pre_flag(data, n2o_col, t_obs_tot, mlo_fit):
     """ 
-    everything with lower n2o than mlo_lim*mlo_fit(frac_year) is flagged 
+    everything with lower n2o than mlo_lim*mlo_fit(frac_year) is flagged (3% cut-off)
     as 'strato' in an initial filtering step 
     """ 
     mlo_lim = 0.97
 
+    # initialise columns to hold strat and trop flags (needs to be done on two lines)
     data = data.assign(strato = np.nan)
     data = data.assign(tropo = np.nan)
 
@@ -121,83 +128,232 @@ def pre_flag(data, n2o_col, t_obs_tot, mlo_fit):
     print('Result of pre-flagging: \n', pre_flagged.value_counts()) # show results of preflagging
     return data, pre_flagged
 
-def filter_strat_trop(data, ref_data, crit, mlo_fit):
+def filter_strat_trop(data, crit):
     """ 
-    Sort data into stratosphere or troposphere based on reference data
+    Reconstruction of filter_strat_trop from C_tools (T. Schuck)
+
+    Sort data into stratosphere or troposphere based on outlier statistics 
+    with respect to measurements eg. at Mauna Loa Observatory
+    
+    Returns dataset with new bool columns 'strato' and 'tropo' 
     
     Parameters: 
         data: DataFrame of data to be sorted 
-        ref_data: DataFrame of reference data
-        crit: substance to be used for categorisation, eg. n2o or sf6 
+        crit: substance to be used for filtering, eg. n2o or sf6 
     """
-    # find column names for caribic data
-    n2o_col = caribic_data.get_col_name('n2o')
-    sf6_col = caribic_data.get_col_name('sf6')
-
-    # choose only rows where sf6 and n2o data exists
-    data = data.dropna(how='any', subset=[n2o_col])
-    data = data.dropna(how='any', subset=[sf6_col])
-
-    # find total observation time as fractional year
-    t_obs_tot = np.array(datetime_to_fractionalyear(data.index, method='exact'))
-
-    # initialise columns to hold strat and trop flags (needs to be done on two lines)
-
-
-    data, pre_flagged = pre_flag(data, n2o_col, t_obs_tot, mlo_fit) # pre-flagging
-
-    # OUTLIER 
+    # OUTLIER: Trop / Strat identification using outlier statistics
     if crit == 'n2o':
-        dir_val = 'n'
+        mlo_fit = get_mlo_fit(n2o_df)
+        ref_data = n2o_df
+
+        n2o_col = caribic_data.get_col_name('n2o') # get column name
+        data = data.dropna(how='any', subset=[n2o_col]) # choose only rows where n2o data exists
+        t_obs_tot = np.array(datetime_to_fractionalyear(data.index, method='exact'))  # find total observation time as fractional year for fctn calls below
+
+        data, pre_flagged = pre_flag(data, n2o_col, t_obs_tot, mlo_fit) # pre-flagging
+
         n2o_mxr = data[n2o_col] # measured n2o mixing ratios
         n2o_d_mxr = data['d_N2O [ppb]']
         # print(data.index, data[n2o_col],  pre_flagged.n2o_flag)
 
         ol_n2o = outliers.find_ol(fct.simple, t_obs_tot, n2o_mxr, n2o_d_mxr, flag = pre_flagged.n2o_pre_flag, 
-                              plot=True, limit=0.1, direction = dir_val)
-        # ^ 4er tuple, 1st ist liste von OL=1, !OL=0
+                              plot=True, limit=0.1, direction = 'n')
+        print('\n OL N2O\n', ol_n2o[0].values)
+        # ^ 4er tuple, 1st ist liste von OL == 1 / 2 / 3, wenn not outlier dann == 0
         data.loc[(ol_n2o[0] != 0), ('strato', 'tropo')] = (True, False)
         data.loc[(ol_n2o[0] == 0), ('strato', 'tropo')] = (False, True)
 
     if crit == 'sf6':
-        dir_val = 'n'
+        mlo_fit = get_mlo_fit(sf6_df, substance='SF6catsMLOm')
+        ref_data = sf6_df
+
+        sf6_col = caribic_data.get_col_name('sf6')
+        data = data.dropna(how='any', subset=[sf6_col]) # choose only rows where sf6 data exists
+        t_obs_tot = np.array(datetime_to_fractionalyear(data.index, method='exact'))
+        data, pre_flagged = pre_flag(data, sf6_col, t_obs_tot, mlo_fit) # pre-flagging
         sf6_mxr = data[sf6_col] # measured n2o mixing ratios
         sf6_d_mxr = data['d_SF6 [ppt]']
 
         ol_sf6 = outliers.find_ol(fct.simple, t_obs_tot, sf6_mxr, sf6_d_mxr, flag = pre_flagged.n2o_pre_flag, 
-                              plot=True, limit=0.1, direction = dir_val)
-        data.loc[(ol_sf6[0] != 0), ('strato', 'tropo')] = (True, False)
-        data.loc[(ol_sf6[0] == 0), ('strato', 'tropo')] = (False, True)
+                              plot=True, limit=0.1, direction = 'n')
+        data.loc[(ol_sf6[0] != 0), ('strato', 'tropo_ol')] = (True, False)
+        data.loc[(ol_sf6[0] == 0), ('strato', 'tropo_ol')] = (False, True)
 
     return data
 
-def filtered_caribic(c_year):
-    print(f'{c_year}')
-    c_data = caribic_data.select_year(c_year)
-    
-    for crit in ['n2o', 'sf6']:
-        if crit=='n2o' and len(get_no_nan(c_data.index, c_data['N2O [ppb]'], c_data['d_N2O [ppb]'])[0]) < 1: # check for valid data
-            print('! no n2o data'); continue
-        try: print(c_data['N2O [ppb]'], '\n cols:', c_data.columns)
-        except: pass
+def get_fct_substance(substance):
+    df_func_dict = {
+        'sf6': fct.simple,
+        'n2o' : fct.simple
+        }
+    return df_func_dict[substance]
 
-        if crit=='sf6' and len(get_no_nan(c_data.index, c_data['SF6 [ppt]'], c_data['d_SF6 [ppt]'])[0]) < 1: # check for valid data
-                print('! no sf6 data'); continue
-        try: print(c_data['SF6 [ppt]'], '\n cols:', c_data.columns)
-        except: pass
+def filter_trop_outliers(data, substance_list):
+    """ 
+    After sorting data into stratospheric and tropospheric, now sort the 
+    tropospheric data into outliers and non-outliers 
+    Parameters:
+        data: pandas (geo)dataframe
+        substance_list: list of strings, substances to be receive a flag 
+    """
+    # take only tropospheric data 
+    for subs in substance_list:
+        subs = caribic_data.get_col_name(subs)
+        if len(get_no_nan(data.index, data[subs], data[subs])[0]) < 1: # check for valid data
+            print(f'no {subs} data'); continue
 
-        return filter_strat_trop(c_data, ref_data, crit, mlo_fit)
+        try: func = get_fct_substance(subs)
+        except: 
+            print('No function found. Using 2nd order poly with simple harm')
+            func = fct.simple
+
+        data_flag = pd.DataFrame(data, columns=['flight', 'timecref', 'year', 'month', 'day', 'strato', 'tropo'])
+        data_flag.columns = [f'fl_{x}' if x in substance_list else x for x in data_flag.columns]
+
+        data_flag[f'ol_{subs}'] = np.nan # outlier ? 
+        data_flag[f'ol_rel_{subs}'] = np.nan # 
+        data_flag[f'fl_{subs}'] = 0 # flag
+
+        # set all strato flags to a value != 0 to exclude them
+        data_flag.loc[data['strato'] == True, f'fl_{subs}'] = -20
+
+        time = np.array(datetime_to_fractionalyear(data.index, method='exact'))
+        mxr = data[subs].tolist()
+        if f'd_{subs}' in data.columns:
+            d_mxr = data[f'd_{subs}'].tolist()
+        else:    # case for integrated values of high resolution data
+            d_mxr = None
+        flag = data_flag[f'fl_{subs}'].tolist()
+        tmp = outliers.find_ol(func, time, mxr, d_mxr, flag, direction='pn',
+                               plot=True, limit=0.1)
+
+        data_flag[f'fl_{subs}'] = tmp[0]  # flag
+        data_flag[f'ol_{subs}'] = tmp[1]  # residual
+
+        data_flag.loc[data_flag['strato'] == True, f'fl_{subs}'] = np.nan
+        data_flag.loc[data_flag['strato'] == True, f'ol_{subs}'] = np.nan
+
+        # no residual value for non-outliers
+        # data_flag.loc[data_flag[f'fl_{subst}'] == 0, f'ol_{subst}'] = np.nan
+
+        fit_result = [func(t, *tmp[3]) for t in time]
+        # print(len(fit_result), len(data_flag))
+        data_flag[f'ol_rel_{subs}'] = data_flag[f'ol_{subs}'] / fit_result
+
+    return data_flag
+
 
 if __name__=='__main__':
-    mlo_fit = get_mlo_fit(n2o_df)
-    ref_data = n2o_df
-    pv_lim=2.
-
     # loop through years of caribic data
-    data_filtered = pd.DataFrame()
-    for c_year in range(2017, 2022): 
-        single_year = filtered_caribic(c_year)
-        data_filtered = pd.concat([data_filtered, single_year])
+    data_filtered = pd.DataFrame() # initialise full dataframe
+    for c_year in range(2005, 2022): 
+        print(f'{c_year}')
+        c_data = caribic_data.select_year(c_year)
+        print('cols:', c_data.columns)
+
+        crit = 'n2o'; n2o_filtered = pd.DataFrame()
+        if len(get_no_nan(c_data.index, c_data['N2O [ppb]'], c_data['d_N2O [ppb]'])[0]) < 1: # check for valid data
+            print('! no n2o data')
+        else:
+
+            n2o_filtered =  filter_strat_trop(c_data, crit)
+            data_filtered = pd.concat([data_filtered, n2o_filtered])
+
+        crit = 'sf6'; sf6_filtered = pd.DataFrame()
+        if crit=='sf6' and len(get_no_nan(c_data.index, c_data['SF6 [ppt]'], c_data['d_SF6 [ppt]'])[0]) < 1: # check for valid data
+                print('! no sf6 data')
+        else: 
+            sf6_filtered =  filter_strat_trop(c_data, crit)
+            data_filtered = pd.concat([data_filtered, sf6_filtered])
 
     data_stratosphere = data_filtered.loc[data_filtered['strato'] == True]
+    print(data_stratosphere.value_counts)
     data_troposphere = data_filtered.loc[data_filtered['tropo'] == True]
+
+    data_trop_outlier = filter_trop_outliers(data_filtered, ['n2o'])
+
+#%% Detrend data for a specific substance wrt free troposphere ? reference data 
+def detrend_substance(data, substance, ref_data, ref_subs, degree=2):
+    """ (redefined from C_tools.detrend_subs)
+    Remove linear trend of viable substances eg. SF6 using reference data 
+    Parameters:
+        data: pandas (geo)dataframe of observations to detrend, index=datetime
+        substance: str, column name of data (e.g. 'SF6 [ppt]')
+        ref_data: pandas (geo)dataframe of reference data to detrend on, index=datetime
+        ref_subs: str, column name of reference data
+    """
+    # ignore reference data earlier and later than two years before/after msmts
+    two_yrs = dt.timedelta(356*2)
+    ref_data_sliced = ref_data[min(data.index)-two_yrs : max(data.index)+two_yrs]
+
+    c_obs = data[substance].values()
+    t_obs =  np.array(datetime_to_fractionalyear(data.index, method='exact'))
+
+    c_ref = ref_data_sliced[ref_subs].values()
+    t_ref = np.array(datetime_to_fractionalyear(ref_data_sliced.index, method='exact'))
+
+    ts_fit = np.polyfit(t_ref, c_ref, degree)
+    c_fit = np.poly1d(ts_fit)
+
+    detrend_correction = c_fit(t_obs) - c_fit(min(t_obs))
+    c_obs_detr = c_obs - detrend_correction
+
+    return c_obs_detr
+
+#%% Plotting Gradient by season
+
+
+def pl_gradient_by_season(data, substance, tropopause='therm', 
+                          min_y=-50, max_y=80, bsize=10, ptsmin=5):
+    """ 
+    Plotting gradient by season using 1D binned data 
+    Parameters:
+        data: pandas (geo)dataframe
+        substance: str, eg. 'SF6 [ppt]'
+        tropopause: str, which tropopause definition to use 
+        min_y, max_y: int, defines longitude range to plot
+        bsize: int, bin size for 1D binning
+        ptsmin: int, minimum number of pts for a bin to be considered 
+    """
+    var = 1 # select_var_list
+    value = 1
+    cf = C_tools.get_op('lt')
+
+    c_obs = data[substance].values()
+    t_obs =  np.array(datetime_to_fractionalyear(data.index, method='exact'))
+
+    nbins = (max_y - min_y) / bsize
+    y_array = min_y + np.arange(nbins) * bsize + bsize * 0.5
+
+    dict_season = {'name_1': 'spring', 'name_2': 'summer', 'name_3': 'autumn', 'name_4': 'winter',
+                   'color_1': 'blue', 'color_2': 'orange', 'color_3': 'green', 'color_4': 'red'}
+
+    for s in set(data['season'].tolist()):
+        df_sub = data.loc[data['season'] == s]
+        y_values = df_sub[yvar].values
+        x_values = df_sub[f'detr_{substance}'].values
+        dict_season[f'bin1d_{s}'] = bin_1d_2d.bin_1d(x_values, y_values, min_y, max_y, bsize)
+
+
+
+#%% 
+mlo_fit = get_mlo_fit(n2o_df)
+ref_data = n2o_df
+data = caribic_data.select_year(2019)
+
+crit = 'n2o'
+n2o_col = caribic_data.get_col_name('n2o') # get column name
+data = data.dropna(how='any', subset=[n2o_col]) # choose only rows where n2o data exists
+t_obs_tot = np.array(datetime_to_fractionalyear(data.index, method='exact'))  # find total observation time as fractional year for fctn calls below
+data, pre_flagged = pre_flag(data, n2o_col, t_obs_tot, mlo_fit) # pre-flagging
+
+n2o_mxr = data[n2o_col] # measured n2o mixing ratios
+n2o_d_mxr = data['d_N2O [ppb]']
+# print(data.index, data[n2o_col],  pre_flagged.n2o_flag)
+
+ol_n2o = outliers.find_ol(fct.simple, t_obs_tot, n2o_mxr, n2o_d_mxr, flag = pre_flagged.n2o_pre_flag, 
+                      plot=True, limit=0.1, direction = 'n')
+# ^ 4er tuple, 1st ist liste von OL=1, !OL=0
+
+data.loc[(ol_n2o[0] != 0), ('strato', 'tropo')] = (True, False)
+data.loc[(ol_n2o[0] == 0), ('strato', 'tropo')] = (False, True)
