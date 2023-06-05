f851047ecd5f6c8eaa3389bad20249843e860e11
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-06-05 17:25:26 +0200

    mini changes

--- time_lag.py
+++ time_lag.py
@@ -17,7 +17,7 @@ from toolpac.conv.times import datetime_to_fractionalyear
 from dictionaries import get_col_name
 
 def calc_time_lags(c_obj, ref_obj, years, substance='sf6', pfx='GHG', 
-                   ref_min=2003, ref_max=2020, plot=True, save=True, verbose=False):
+                   ref_min=2003, ref_max=2020, plot_yr=False, plot_all=True, save=True, verbose=False):
     """ Calculate and plot time lag for caribic data wrt mauna loa msmts for specified years 
     c_obj : (Caribic) type object, needs to have attr data
     re_obj : 
@@ -57,7 +57,7 @@ def calc_time_lags(c_obj, ref_obj, years, substance='sf6', pfx='GHG',
             lags.append((lag))
 
         if verbose: print('length of lags and mean for {yr} : {len(lags)} {np.nanmean(np.array(lags))}')
-        if plot: plot_time_lags(df, lags, [yr], ref_min, ref_max, subs)
+        if plot_yr: plot_time_lags(df, lags, [yr], ref_min, ref_max, subs)
 
         all_lags.extend(lags)
 
@@ -66,6 +66,7 @@ def calc_time_lags(c_obj, ref_obj, years, substance='sf6', pfx='GHG',
 
     col_name = f'lag_{substance} [yr]'
     df_lags = pd.DataFrame(all_lags, index = lag_index, columns = [col_name])
+    if plot_all: plot_time_lags(df_lags, all_lags, years)
 
     if save: # save in data dictionary 
             if f'lag_{pfx}' not in c_obj.data.keys(): # new lag df for this pfx, create the dict entry  
@@ -86,8 +87,8 @@ def plot_time_lags(df, lags, years, ref_min=2003, ref_max=2020, subs = 'sf6'):
               dt.datetime(min(years), 1, 1), 
               dt.datetime(max(years), 12, 31), 
               'r', ls='dashed', label = 'Mean')
-    plt.title('CARIBIC {} time lag {} wrt. MLO {} - {}'.format(
-        subs, df.index.year[0], ref_min, ref_max))
+    plt.title('{} CARIBIC time lag wrt. MLO {} - {}'.format(
+        subs.upper(), ref_min, ref_max))
     plt.ylabel('Time lag [yr]')
     plt.xlabel('CARIBIC Measurement time')
     plt.legend()
