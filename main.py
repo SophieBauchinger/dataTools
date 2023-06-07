a7f749ad12fac6a89899a03a989ddaed8a8c7e52
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-06-07 14:34:43 +0200

    smol changes

--- main.py
+++ main.py
@@ -6,10 +6,9 @@
 Main script for Caribic measurement analysis routine.
 
 Substances in Caribic data:
-'GHG':    ['ch4', 'co2', 'n2o', 'sf6']
-'INT':    ['co', 'o3', 'h2o', 'no', 'noy', 'co2', 'ch4', 'f11', 'f12', 'n2o']
-'INT2':   ['noy', 'no', 'ch4', 'co', 'co2', 'h2o', 'n2o', 'o3'] 
-
+'GHG':    ['ch4', 'co2',        'n2o', 'sf6']
+'INT':    ['ch4', 'co2', 'co' ,             'o3' , 'noy', 'no' , 'h2o']
+'INT2':   ['ch4', 'co2', 'co' , 'n2o',      'o3' , 'noy', 'no' , 'h2o', 'f11', 'f12']
 """
 from data_classes import Caribic, Mozart, Mauna_Loa, Mace_Head
 from time_lag import calc_time_lags
@@ -19,6 +18,7 @@ from gradients import plot_gradient_by_season
 from filter_outliers import filter_strat_trop, filter_trop_outliers
 from detrend import detrend_substance
 from plot import plot_scatter_global, plot_global_binned_1d, plot_global_binned_2d, plot_1d_LonLat, plot_local
+from eqlat_plots import plot_eqlat_deltheta
 
 #%% Get data
 year_range = range(1980, 2021)
@@ -29,8 +29,11 @@ mhd = Mace_Head() # only 2012 data available
 mzt = Mozart(year_range) # only available up to 2008
 
 # examples for creating new objects with only certain year / flight number:
-c_2012 = caribic.sel_year(2008)
+c_2008 = caribic.sel_year(2008)
 c_fl340 = caribic.sel_flight(340)
+
+c_yr08_to_12 = caribic.sel_year(*range(2008, 2012))
+c_fl340_to_360 = caribic.sel_flight(*range(340, 360))
 
 #%% Plot data
 import matplotlib.pyplot as plt
@@ -76,7 +79,7 @@ calc_time_lags(caribic, mlo_data['n2o'], range(2005, 2020), substance = 'n2o', p
 calc_time_lags(caribic, mlo_data['n2o'], range(2005, 2020), substance = 'n2o', plot_all=lag_plot)
 calc_time_lags(caribic, mlo_data['co2'], range(2005, 2020), substance = 'co2', pfx='INT2', plot_all=lag_plot)
 
-# to delete the newly created attributes use
+# to delete the newly created attributes may use
 # del caribic.data['lag_GHG'] etc
 
 #%% Detrend
@@ -84,9 +87,9 @@ for subs in ['sf6', 'n2o', 'co2', 'ch4']:
     detrend_substance(caribic, subs, mlo_data[subs])
 
 #%% Plot gradients 
-for pfx in caribic.pfxs:
+for pfx in ['INT2']:# caribic.pfxs:
     for subs in substance_list(pfx):
-        plot_gradient_by_season(caribic, subs, pfx)
+        plot_gradient_by_season(caribic, subs, 'INT2', tp='pvu', pvu = 2.0,)
 
 #%% Filter tropospheric / stratospheric data points based on n2o mixing ratio wrt Mauna Loa data 
 for pfx in caribic.pfxs: 
@@ -97,3 +100,5 @@ for pfx in caribic.pfxs:
         if subs in ['sf6', 'n2o', 'co2', 'ch4']:
             filter_trop_outliers(caribic, subs, pfx, crit=subs)
 
+#%% Eq. lat vs potential temperature wrt tropopause
+plot_eqlat_deltheta(caribic, c_pfx='INT2', subs='n2o')
