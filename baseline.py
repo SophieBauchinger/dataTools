2d1264baa6aabe47f930ca9add60574c4e5d43bb
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-07-11 15:52:27 +0200

    big changes n stuff

--- baseline.py
+++ baseline.py
@@ -17,112 +17,120 @@ import warnings; warnings.filterwarnings("ignore", category=UserWarning,
                                          module='matplotlib')
 
 from toolpac.outliers import outliers
-from toolpac.outliers import ol_fit_functions as fct
 from toolpac.conv.times import datetime_to_fractionalyear
 
-from filter import 
+from dictionaries import get_col_name, get_fct_substance
 
-#!!! TO-DO:
-    # implement different trop / strat filter to be used to extract trop data only
-
-def baseline_filter(glob_obj, subs, c_pfx='GHG', crit='n2o', direction='p',
-                         ref_obj=None, save=True, plot=True, plot_strat=False,
-                         ax=None):
+def baseline_filter(glob_obj, subs='co2', c_pfx='GHG', tp_def='chem', crit='n2o', 
+                    coord='pt', pvu=3.5, direction='p', ref_obj=None, save=True, 
+                    plot=True, plot_strat=True, ol_limit=0.1, **kwargs):
     """
     After sorting data into stratospheric and tropospheric, now sort the
     tropospheric data into outliers and non-outliers (in both directions)
     Parameters:
-        glob_obj (GlobalData): e.g. caribic
+        glob_obj (GlobalData): Caribic
         subs (str): substances to flag, eg. 'n2o'
+        tp_def (str): tropopause definition: 'chem', 'dyn', 'therm'
         c_pfx (str): caribic data prefix, eg. 'GHG'
-        crit (str): criterion for strat / trop sorting (eg. 'n2o')
+        crit (str): 'n2o', 'o3'
+        coord (str): 'dp', 'pt', 'z'
+        pvu (float): 1.5, 2.0, 3.5
+        direction (str): 'p', 'n', 'pn'
         ref_obj (LocalData): reference data for strat / trop sorting if needed
         save (bool): adds filtered data to glob_obj
     """
     state = f'filter_trop_outliers: subs={subs}, c_pfx={c_pfx}, crit={crit}\n'
     # check availability of tropospheric filtered datasets
     if not glob_obj.source == 'Caribic': 
-        raise Exception(state+'Please supply an instance of CARIBIC data.')
-    else:
-        trop_crits = [x for x in glob_obj.data[c_pfx].columns if x.startswith('tropo')]
-        if len(trop_crits) > 0 and f'tropo_{crit}' in glob_obj.data[c_pfx].columns:
-            data = glob_obj.data[c_pfx]
-        elif len(trop_crits) > 0 and f'tropo_{crit}' not in glob_obj.data[c_pfx].columns:
-            try: 
-                data = filter_strat_trop(glob_obj, ref_obj=ref_obj, crit=crit, 
-                                         c_pfx=c_pfx, plot=False)
-            except: 
-                data = glob_obj.data[c_pfx]
-                crit = trop_crits[0][5:]
-                print(state+f'Cannot use chosen crit, so using {crit} instead')
-        else: 
-            data = filter_strat_trop(glob_obj, ref_obj=ref_obj, crit=crit, 
-                                     c_pfx=c_pfx, plot=False)
-        # now take only data that has been flagged as tropospheric 
-        strato = data[data[f'strato_{crit}'] == True]
-        data = data[data[f'tropo_{crit}'] == True]
-    
-    if isinstance(subs, str): subs = [subs]
-    for subs in subs:
-        substance = get_col_name(subs, glob_obj.source, c_pfx)
-        if substance not in data.columns:
-            raise Exception(state+f'{substance} not found in {glob_obj.source} {c_pfx}')
-    
-        # check for valid data for that substance
-        if len(get_no_nan(data.index, data[substance], data[substance])[0]) < 1:
-            raise Exception(state+f'No non-nan {subs} data')
-    
-        # Get outlier fit function
-        try: func = get_fct_substance(subs)
-        except:
-            print(f'No fit function found for {subs}. Using 2nd order poly with simple harm. {c_pfx}')
-            func = fct.simple
-    
-        # create output dataframe, initiate values
-        data_flag = pd.DataFrame(data, columns=['Flight number', f'{substance}',
-                                                f'strato_{crit}', f'tropo_{crit}'])
-        data_flag[f'ol_{subs}'] = np.nan # outlier
-        data_flag[f'ol_rel_{subs}'] = np.nan # residual
-        # data_flag[f'fl_{subs}'] = 0 # flag
-        # flag = data_flag[f'fl_{subs}'].tolist()
-    
-        time = np.array(datetime_to_fractionalyear(data.index, method='exact'))
-        mxr = data[substance].tolist()
-        if f'd_{substance}' in data.columns:
-            d_mxr = data[f'd_{substance}'].tolist()
-        else: d_mxr = None # integrated values of high resolution data
-    
-        tmp = outliers.find_ol(func, time, mxr, d_mxr, flag=None, 
-                               direction=direction, verbose=False,
-                               plot=not(plot), limit=0.1, ctrl_plots=not(plot))
+        raise Exception(state+'Need a Caribic instance.')
 
-        data_flag[f'fl_{subs}'] = tmp[0]  # flag
-        data_flag[f'ol_{subs}'] = tmp[1]  # residual
-        data_flag[f'ol_rel_TMP_{subs}'] = tmp[2] # warning
-        fit_result = [func(t, *tmp[3]) for t in time] # popt1
-        data_flag[f'ol_rel_{subs}'] = data_flag[f'ol_{subs}'] / fit_result # residuals
-    
-        if plot: 
-            if not ax: 
-                fig, ax = plt.subplots(dpi=200)
-                plt.title(f'{state}')
-            ax.scatter(data_flag[data_flag[f'fl_{subs}'] != 0].index, 
-                       data_flag[data_flag[f'fl_{subs}'] != 0][substance],
-                        c='xkcd:orange',  marker='.', zorder=1, label='pollution')
-            ax.scatter(data_flag[data_flag[f'fl_{subs}'] == 0].index, 
-                       data_flag[data_flag[f'fl_{subs}'] == 0][substance],
-                        c='xkcd:grey',  marker='.', zorder=1, label='background')
-            ax.plot(data_flag.index, fit_result, 
-                    label='background fit', c='k')
-            if plot_strat:
-                ax.scatter(strato.index, strato[substance],
-                            c='xkcd:baby blue', lw=0.5, label=f'stratospheric ({crit})', 
-                            zorder=0,  marker='+')
-            ax.set_ylabel(substance)
-            ax.set_xlabel('Time delta')
-            if not ax: plt.legend(); plt.show()
-    
-        # no residual value for non-outliers
-        # data_flag.loc[data_flag[f'fl_{subs}'] == 0, f'ol_{subs}'] = np.nan
+    # Tropospheric data
+    tropo_obj, strato_obj = glob_obj.data_filter(tp_def)
+
+    # tropo_obj, strato_obj = glob_obj.data_filter(filter_type=tp_def, c_pfx=c_pfx,
+    #                                  crit=crit, coord=coord, ref_obj=ref_obj, **kwargs)
+    print('tropo_obj.data[c_pfx] in bl 55 - baseline_filter\n', tropo_obj.data[c_pfx])
+
+    if c_pfx not in tropo_obj.pfxs: 
+        raise KeyError('Filtering did not result in desired cols being created.')
+
+
+    # t_col = [x for x in tropo_obj.data[c_pfx].columns if x.startswith('tropo')][0]
+    data = tropo_obj.data[c_pfx] # [tropo_obj.data[c_pfx] #[t_col] == True]
+    data.sort_index(inplace=True)
+
+    # Stratospheric data
+    # strato_obj = glob_obj.data_filter(filter_type=tp_def, tropo_strato = 'strato', c_pfx=c_pfx, 
+    #                                   crit=crit, coord=coord, ref_obj=ref_obj, **kwargs)
+    # s_col = [x for x in strato_obj.data[c_pfx].columns if x.startswith('strato')][0]
+    strato = strato_obj.data[c_pfx] # [strato_obj.data[c_pfx][s_col] == True]
+    strato.sort_index(inplace=True)
+
+    # if isinstance(subs, str): substances = [subs]
+    # elif isinstance(subs, list): substances = subs
+
+    # for subs in substances:
+    substance = get_col_name(subs, glob_obj.source, c_pfx)
+    if substance not in data.columns:
+        print(state+f'{substance} not found in {c_pfx}') # ; continue
+
+    print(data)
+
+    data_flag = pd.DataFrame(data, columns=['Flight number', f'{substance}'])
+    time = np.array(datetime_to_fractionalyear(data.index, method='exact'))
+    mxr = data[substance].tolist()
+    if f'd_{substance}' in data.columns:
+        d_mxr = data[f'd_{substance}'].tolist()
+    else: d_mxr = None # integrated values of high resolution data
+
+    func = get_fct_substance(subs)
+    tmp = outliers.find_ol(func, time, mxr, d_mxr, flag=None, # here
+                           direction=direction, verbose=False,
+                           plot=not(plot), limit=ol_limit, ctrl_plots=not(plot))
+
+    data_flag[f'fl_{subs}'] = tmp[0]  # flag
+    data_flag[f'ol_{subs}'] = tmp[1]  # residual
+    # data_flag[f'ol_rel_TMP_{subs}'] = tmp[2] # warning
+    fit_result = [func(t, *tmp[3]) for t in time] # popt1
+    data_flag[f'ol_rel_{subs}'] = data_flag[f'ol_{subs}'] / fit_result # residuals
+
+    if plot: 
+        plot_BLfilter_time(subs, tp_def, crit, coord, pvu, c_pfx, data_flag, 
+                               substance, strato, plot_strat, fit_result, **kwargs)
+
+    # no residual value for non-outliers
+    data_flag.loc[data_flag[f'fl_{subs}'] == 0, f'ol_{subs}'] = np.nan
 
     return data_flag
+
+def plot_BLfilter_time(subs, tp_def, crit, coord, pvu, c_pfx, data_flag, 
+                       substance, strato, plot_strat, fit_result, **kwargs):
+    description = f'{tp_def.upper()}'
+    if tp_def == 'chem': description += f' ({crit})'
+    elif tp_def == 'dyn': description += f' ({pvu}, {coord})'
+    else: description += f' ({coord})'
+    
+    if 'ax' not in kwargs.keys(): 
+        fig, ax = plt.subplots(dpi=200)
+        plt.title(f'Baseline filter of {subs.upper()} in {c_pfx} - ' + description)
+    else: 
+        ax = kwargs['ax']
+        ax.set_title(description)
+
+    ax.scatter(data_flag[data_flag[f'fl_{subs}'] != 0].index, 
+               data_flag[data_flag[f'fl_{subs}'] != 0][substance],
+                c='xkcd:orange',  marker='.', zorder=1, label='extr. events')
+    ax.scatter(data_flag[data_flag[f'fl_{subs}'] == 0].index, 
+               data_flag[data_flag[f'fl_{subs}'] == 0][substance],
+                c='xkcd:blue',  marker='.', zorder=1, label='base')
+    if plot_strat:
+        ax.scatter(strato.index, strato[substance],
+                    c='xkcd:grey', label='strato', 
+                    zorder=0,  marker='.')
+    ax.plot(data_flag.index, fit_result, 
+            label='baseline', c='k', zorder=2)
+    ax.set_ylabel(substance)
+    ax.set_xlabel('Time delta')
+    if 'ax' not in kwargs.keys(): 
+        fig.autofmt_xdate()
+        plt.legend(); plt.show()
