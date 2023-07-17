fb061f00d5e28ee1ebba2db9ea2eadb5ce118117
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-07-17 09:03:32 +0200

    reoganise functions, subplot functionality

--- plot/eqlat.py
+++ plot/eqlat.py
@@ -10,15 +10,119 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 
-from toolpac.calc import bin_1d_2d #!!!
-from dictionaries import get_col_name
+import toolpac.calc.binprocessor as bp
+
+from dictionaries import get_col_name, get_h_coord, get_v_coord, dict_season, substance_list
 from detrend import detrend_substance
-from tools import subs_merge, make_season
+from tools import make_season, coordinate_tools
 from data import Mauna_Loa
 
-def plot_eqlat_deltheta(c_obj, subs='n2o', c_pfx='INT2', tp = 'therm', pvu=2.0,
-                        x_bin=None, y_bin=None, x_source='ERA5', vlims=None,
-                        detr=False, note=None):
+#%% 2D plotting
+def get_right_data(c_obj, subs='n2o', c_pfx='INT2', detr=True):
+    substance = get_col_name(subs, c_obj.source, c_pfx)
+    if substance is None: 
+        pfxs_avail = [pfx for pfx in c_obj.pfxs if subs in substance_list(pfx)]
+        if len(pfxs_avail)==0: print('No {subs} data available')
+        else: c_pfx = input(f'No {c_pfx} data found for binning. Choose from {pfxs_avail}')
+        if c_pfx not in c_obj.data.keys(): return
+        substance = get_col_name(subs, c_obj.source, c_pfx)
+    
+    if c_obj.source == 'Caribic': data = c_obj.data[c_pfx]
+    else: data = c_obj.df
+    data['season'] = make_season(data.index.month) # 1 = spring etc
+
+    if detr and not f'detr_{substance}' in data.columns:
+        try: 
+            c_obj.detrend_substance(subs, Mauna_Loa(c_obj.years, subs), save=True)
+            data = c_obj.data[c_pfx]
+            substance = f'detr_{substance}'
+        except: print('Detrending not successful, proceeding with original data.')
+    else: substance = f'detr_{substance}'
+    
+    return data, substance, c_pfx
+
+def seasonal_binning(data, substance, y_bin, y_coord, x_coord, x_bin, vlims):
+    vmin_list, vmax_list = [], []; out_dict = {}
+
+    # calculate binned output per season
+    for s in set(data['season'].tolist()):
+        df = data[data['season'] == s]
+        x = np.array(df[x_coord])
+        y = np.array(df[y_coord])
+        xbmin, xbmax, xbsize = np.nanmin(x), np.nanmax(x), x_bin
+        ybmin, ybmax, ybsize = np.nanmin(y), np.nanmax(y), y_bin
+
+        bin_equi2d = bp.Bin_equi2d(xbmin, xbmax, xbsize, ybmin, ybmax, ybsize)
+        out = bp.Simple_bin_2d(np.array(df[substance]), x, y, bin_equi2d)
+        out_dict[s] = out
+        vmin_list.append(np.nanmin(out.vmean))
+        vmax_list.append(np.nanmax(out.vmean))
+
+    if not vlims: vmin, vmax = (np.nanmin(vmin_list), np.nanmax(vmax_list))
+    else: vmin, vmax = vlims[0], vlims[1]
+
+    return out_dict, vmin, vmax
+
+def plot_parameters():
+    """ x_coord, y_coord, x_lims, y_lims, x_label, y_label """
+
+def plot_2d(bin2d_inst, x_coord, y_coord, ax, season, note,
+            x_lims, y_lims, vlims, x_label, y_label):
+    """ Plot bin2d output onto given axis """
+    ax.set_title(dict_season()[f'name_{season}'])
+    if note: ax.text(x_lims[0]*0.9, y_lims[1]*0.85, note, style='italic',
+                     bbox={'facecolor':'white'})
+    cmap = plt.cm.viridis # create colormap
+    norm = Normalize(*vlims) # normalise color map to set limits
+
+    # =============================================================================
+    vmean_tot = np.nanmean(bin2d_inst.vmean)
+    norm = Normalize(*vlims/ vmean_tot)
+    bin2d_inst.vmean = bin2d_inst.vmean / vmean_tot
+    # =============================================================================
+
+
+    img = ax.imshow(bin2d_inst.vmean.T, cmap = cmap, norm=norm,
+                    aspect='auto', origin='lower',
+                    extent=[bin2d_inst.binclassinstance.xbmin, 
+                            bin2d_inst.binclassinstance.xbmax, 
+                            bin2d_inst.binclassinstance.ybmin, 
+                            bin2d_inst.binclassinstance.ybmax])
+    ax.set_xlabel(x_label); ax.set_xlim(*x_lims)
+    ax.set_ylabel(y_label); ax.set_ylim(*y_lims)
+    return img
+
+def plot_variability(bin2d_inst, x_coord, y_coord, ax, season, note,
+            x_lims, y_lims, vlims, x_label, y_label):
+    ax.set_title(dict_season()[f'name_{season}'])
+    if note: ax.text(x_lims[0]*0.9, y_lims[1]*0.85, note, style='italic',
+                     bbox={'facecolor':'white'})
+    cmap = plt.cm.viridis # create colormap
+    norm = Normalize(*vlims) # normalise color map to set limits
+    
+    vmean_tot = np.nanmean(bin2d_inst.vmean)
+    bin2d_inst.vmean.T - vmean_tot
+    
+    print(vmean_tot)
+    
+    img = ax.imshow(bin2d_inst.vmean.T- vmean_tot, cmap = cmap, norm=norm,
+                    aspect='auto', origin='lower',
+                    extent=[bin2d_inst.binclassinstance.xbmin, 
+                            bin2d_inst.binclassinstance.xbmax, 
+                            bin2d_inst.binclassinstance.ybmin, 
+                            bin2d_inst.binclassinstance.ybmax])
+    ax.set_xlabel(x_label); ax.set_xlim(*x_lims)
+    ax.set_ylabel(y_label); ax.set_ylim(*y_lims)
+    return img
+
+def plot_difference(bin2d_inst1, bin2d_inst2, x_coord, y_coord, ax, season, note,
+            x_lims, y_lims, vlims, x_label, y_label):
+    """ Plot difference between plots """
+    pass
+
+def plot_2d_binned(c_obj, subs='n2o', c_pfx='INT2', ycoord = 'pt', xcoord = 'eql',
+                   tp_def = 'dyn', pvu=3.5,
+                   x_bin=10, y_bin=None, vlims=None, detr=True, note=None):
     """ Plot binned mxr on EqL vs. pot.T or 
     
     Creates plots of equivalent latitude versus potential temperature or height
@@ -31,135 +135,34 @@ def plot_eqlat_deltheta(c_obj, subs='n2o', c_pfx='INT2', tp = 'therm', pvu=2.0,
         tp (str): tropopause definition. 'therm', 'dyn', 'z' or 'pvu'
         pvu (float): potential vorticity set as tropopause definition. 1.5, 2.0 or 3.5
     """
-    if c_obj.source == 'Caribic': data = c_obj.data[c_pfx]
-    else: data = c_obj.df
+    data, substance, c_pfx = get_right_data(c_obj, subs, c_pfx, detr)
+    y_coord, y_label, x_coord, x_label = coordinate_tools(
+        tp_def=tp_def, c_pfx=c_pfx, ycoord=ycoord, pvu=pvu, xcoord=xcoord)
 
-    substance = get_col_name(subs, c_obj.source, c_pfx)
-    if detr: 
-        if not f'detr_{substance}' in data.columns:
-            try: 
-                detrend_substance(c_obj, subs, Mauna_Loa(c_obj.years, subs), save=True)
-                data = c_obj.data[c_pfx]
-                substance = f'detr_{substance}'
-            except: print('Detrending not successful, proceeding with original data.')
-        else: substance = f'detr_{substance}'
-
-# Old code: 
-# =============================================================================
-#     if not f'{subs}_data' in c_obj.data.keys():
-#         detrend_substance(c_obj, subs, Mauna_Loa(c_obj.years, subs))
-#         subs_merge(c_obj, subs, save=True, detr=True)
-# 
-#     try:
-#         data = c_obj.data[f'{subs}_data']
-#         substance = get_col_name(subs, c_obj.source, 'GHG')
-#         if detr and 'delta_'+substance in data.columns:
-#             substance = 'delta_'+substance
-#     except:
-#         if c_obj.source == 'Caribic':
-#             data = c_obj.data[c_pfx]
-#             substance = get_col_name(subs, c_obj.source, c_pfx)
-#         elif c_obj.source == 'Mozart': data = c_obj.df
-#         else: raise('Could not find the specified data set. Check your input')
-# =============================================================================
-
-    data['season'] = make_season(data.index.month) # 1 = spring etc
-    dict_season = {'name_1': 'Spring (MAM)', 'name_2': 'Summer (JJA)',
-                   'name_3': 'Autumn (SON)', 'name_4': 'Winter (DJF)',
-                   'color_1': 'blue', 'color_2': 'orange',
-                   'color_3': 'green', 'color_4': 'red'}
-
-    # Get column name for y axis depending on function parameter
-    if tp == 'z':
-        # height relative to the tropopause in km: H_rel_TP (chem OZONE TP)
-        y_coord = 'int_CARIBIC2_H_rel_TP [km]'
-        y_label = '$\Delta$z [km]'
-        if not y_bin: y_bin = 0.25 # km
-    elif tp == 'pvu':
-        # pot temp difference to potential vorticity surface
-        y_coord = 'int_ERA5_D_{}_{}PVU_BOT [K]'.format(str(pvu)[0], str(pvu)[2])
-        if not y_bin: y_bin = 5 # K
-        y_label = f'$\Delta\Theta$ ({pvu} PVU - ERA5) [K]'
-    elif tp =='therm':
-        #  potential T. difference relative to thermal tropopause from ECMWF
-        y_coord = 'int_pt_rel_sTP_K [K]'
-        if not y_bin: y_bin = 5 # K
-        y_label = f'$\Delta\Theta$ ({tp} - ECMWF) [K]'
-    elif tp == 'dyn':
-        #  potential T difference rel. to dynamical (PV=3.5PVU) tp from ECMWF
-        y_coord = 'int_pt_rel_dTP_K [K]'
-        if not y_bin: y_bin = 10 # K
-        y_label = f'$\Delta\Theta$ ({tp} - ECMWF) [K]'
-    else:
-        y_coord = 'p [mbar]'
-        if not y_bin: y_bin = 40 # mbar
-        y_label = 'Pressure [mbar]'
-
-    if x_source=='ERA5':
-        x_coord = 'int_ERA5_EQLAT [deg N]' # Equivalent latitude (ERA5)
-    elif x_source=='ECMWF':
-        x_coord = 'int_eqlat [deg]' # equivalent latitude in °N from ECMWF
-    else: raise('No valid source for equivalent latitude given. ')
-
-    # set bin sizes and plot limits
-    x_label = f'Eq. latitude ({x_source}) [°N]'
-    if not x_bin: x_bin = 10 # °N
-
-    if c_obj.source == 'Mozart':
-        y_coord = 'PS'; y_label='Surface Pressure [Pa]'; y_bin = 5e3
-
-    print(data.columns)
+    y_bins = {'z' : 0.25, 'pt' : 10, 'p' : 40}
+    if not y_bin: y_bin = y_bins[ycoord]
 
     y_lims = np.nanmin(data[y_coord])-y_bin, np.nanmax(data[y_coord])+y_bin
     x_lims = (-90-x_bin, 90+x_bin)
 
-    vmin_list, vmax_list = [], []; out_dict = {}
+    out_dict, vmin, vmax = seasonal_binning(data, substance, 
+                                            y_bin, y_coord, x_coord, x_bin, vlims)
 
-    # calculate binned output per season
-    for s in set(data['season'].tolist()):
-        df = data[data['season'] == s]
-
-        if not c_obj.source == 'Mozart': x = np.array(df[x_coord])
-        else: x = np.array(df.geometry.x); x_label='latitude'
-        y = np.array(df[y_coord])
-        xbmin, xbmax, xbsize = np.nanmin(x), np.nanmax(x), x_bin
-        ybmin, ybmax, ybsize = np.nanmin(y), np.nanmax(y), y_bin
-
-        out = bin_1d_2d.bin_2d(np.array(df[substance]), x, y,
-                               xbmin, xbmax, xbsize, ybmin, ybmax, ybsize)
-        out_dict[s] = out
-        vmin_list.append(np.nanmin(out.vmean))
-        vmax_list.append(np.nanmax(out.vmean))
-
-    if vlims: vmin, vmax = vlims[0], vlims[1]
-    else: vmin = np.nanmin(vmin_list); vmax = np.nanmax(vmax_list)
-
-    # create parent plot
+    # Create plots for all seasons separately
     f, axs = plt.subplots(2, 2, dpi=250, figsize=(9,7))
-
     for s, ax in zip(set(data['season'].tolist()), axs.flat): # flatten axs array
         out = out_dict[s] # take binned data for current season
-        ax.set_title(dict_season[f'name_{s}'])
-        if note: ax.text(x_lims[0]*0.9, y_lims[1]*0.85, note, style='italic',
-                         bbox={'facecolor':'white'})
-        cmap = plt.cm.viridis # create colormap
-        norm = Normalize(vmin, vmax) # normalise color map to set limits
-        img = ax.imshow(out.vmean.T, cmap = cmap, norm=norm,
-                        aspect='auto', origin='lower',
-                        extent=[out.xbmin, out.xbmax, out.ybmin, out.ybmax])
-        ax.set_xlabel(x_label); ax.set_xlim(*x_lims)
-        ax.set_ylabel(y_label); ax.set_ylim(*y_lims)
-
-    # f.suptitle('{} from {} {} between {} - {}. Grid: {}{} x {}{}'.format(
-    #     substance, c_obj.source, c_pfx, c_obj.years[0], c_obj.years[-1],
-    #     x_bin, x_label[x_label.find('[')+1 : x_label.find(']')],
-    #     y_bin, y_label[y_label.find('[')+1 : y_label.find(']')]))
-
+        img = plot_2d(out, x_coord, y_coord, ax, s, note, 
+                      x_lims, y_lims, (vmin, vmax), x_label, y_label)
+        
+        plot_variability(out, x_coord, y_coord, ax, s, note, 
+                      x_lims, y_lims, (vmin, vmax), x_label, y_label)
+        
     f.subplots_adjust(right=0.9)
     plt.tight_layout(pad=2.5)
     cbar = f.colorbar(img, ax = axs.ravel().tolist(), aspect=30, pad=0.09)
     xlabel = subs.upper() + ' ' + substance[substance.find('['):substance.find(']')+1]
-    if detr: xlabel = '$\Delta $'+xlabel
+    if detr: xlabel = '$\Delta$ '+xlabel
     cbar.ax.set_xlabel(xlabel)
     plt.show()
 
