2cb674e57aed9091d80a424ba1e54a53505bc601
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2023-10-16 19:56:23 +0200

    updated to work with previous updates

--- plot/eqlat.py
+++ plot/eqlat.py
@@ -9,27 +9,246 @@ tropopause (in km or K) versus equivalent latitude (in deg N)
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
+import pandas as pd
+import itertools
 
 import toolpac.calc.binprocessor as bp
 
-from dictionaries import get_col_name, get_h_coord, get_v_coord, dict_season, substance_list
-from tools import make_season, coordinate_tools
+import dictionaries as dcts
+import tools
+# from dictionaries import get_col_name, get_h_coord, get_v_coord, dict_season, substance_list
+# from tools import make_season, coordinate_tools
+
+class Bin2dPlotter():
+    """ """ 
+    def __init__(self, df, detr=False, 
+                 subs_params=dict(), 
+                 x_params=dict(hcoord='eql'), 
+                 y_params=dict(vcoord='pt', tp_def='dyn', rel_to_tp=True),
+                 **kwargs):
+        """ Initialise class instances. """
+        df['season'] = tools.make_season(df.index.month)
+        self.data = {'df' : df} # dataframe
+        self.detr = detr
+        self.substances = [s for s in dcts.get_substances(**subs_params)
+                           if s.col_name in df.columns]
+        self.x_coordinates = [s for s in dcts.get_coordinates(**x_params)
+                              if s.col_name in df.columns]
+        self.y_coordinates = [s for s in dcts.get_coordinates(**y_params)
+                              if s.col_name in df.columns]
+        self.kwargs = kwargs
+        self.check_input()
+
+    def __repr__(self):
+        return '<Bin2dPlotter>\n - subs: {}\n - xcoords: {}\n - ycoords: {}'.format(
+            [f'{s.short_name} ({s.ID})' for s in self.substances],
+            self.x_coordinates, self.y_coordinates)
+
+    @property
+    def df(self) -> pd.DataFrame:
+        return self.data['df']
+
+    def check_input(self) -> bool: 
+        """ Check if any subs, x, y columns are in data. """
+        if not any([(s.col_name in self.df.columns) for s in self.substances]): 
+            raise KeyError('No substances found in data that fit the parameters. ')
+        if not any([(c.col_name in self.df.columns) for c in self.x_coordinates]): 
+            raise KeyError('No x-coordinate found in data that fit the parameters. ')
+        if not any([(c.col_name in self.df.columns) for c in self.y_coordinates]): 
+            raise KeyError('No y-coordinate found in data that fit the parameters. ')
+        return True
+
+    def get_limits(self, subs, xcoord, ycoord) -> (tuple, tuple, tuple):
+        """ Check kwargs for limits, otherwise set default values """
+        if 'vlims' in self.kwargs:
+            vlims = self.kwargs.get('vlims')
+        else: 
+            try: vlims = dcts.get_default_vlims(subs.short_name, detr=self.detr)
+            except KeyError: vlims = (np.nanmin(self.df[subs.col_name]), np.nanmax(self.df[subs.col_name]))
+        
+        if 'xlims' in self.kwargs:
+            xlims = self.kwargs.get('xlims')
+        else: 
+            xlims = (-90, 90)
+            # xlims = (np.nanmin(self.df[xcoord.col_name]), np.nanmax(self.df[xcoord.col_name]))
+
+        if 'ylims' in self.kwargs:
+            ylims = self.kwargs.get('ylims')
+        else: 
+            ylims = (np.nanmin(self.df[ycoord.col_name]), np.nanmax(self.df[ycoord.col_name]))
+        
+        return vlims, xlims, ylims
+
+    def get_bins(self, subs, xcoord, ycoord) -> (float, float):
+        """ Get bin size for x- and y-coordinates. """
+        vlims, xlims, ylims = self.get_limits(subs, xcoord, ycoord)
+        
+        x_bin = 10 if not self.kwargs.get('x_bin') else self.kwargs.get('x_bin')
+        y_bin = max(ylims)/10 if not self.kwargs.get('y_bin') else self.kwargs.get('y_bin')
+
+        return x_bin, y_bin
+
+    def bin_seasonal(self, subs, xcoord, ycoord, 
+                     bin_equi2d = None, 
+                     x_bin=None, y_bin=None) -> dict:
+        """ Bin the dataframe per season. """
+        out_dict = {}
+        if not x_bin: 
+            x_bin = self.get_bins(subs, xcoord, ycoord)[0]
+        if not y_bin: 
+            y_bin = self.get_bins(subs, xcoord, ycoord)[1]
+
+        # calculate binned output per season
+        for s in set(self.df['season'].tolist()):
+            df = self.df[self.df['season'] == s]
+            x = np.array(df[xcoord.col_name])
+            y = np.array(df[ycoord.col_name])
+
+            xbmin, xbmax, xbsize = np.nanmin(x), np.nanmax(x), x_bin
+            ybmin, ybmax, ybsize = np.nanmin(y), np.nanmax(y), y_bin
+
+            if not bin_equi2d: 
+                bin_equi2d = bp.Bin_equi2d(xbmin, xbmax, xbsize, ybmin, ybmax, ybsize)
+
+            if self.detr: 
+                out = bp.Simple_bin_2d(np.array(df['detr_'+subs.col_name]), x, y, bin_equi2d)
+            else: out = bp.Simple_bin_2d(np.array(df[subs.col_name]), x, y, bin_equi2d)
+            
+            out = bp.Simple_bin_2d(np.array(df[subs.col_name]), x, y, bin_equi2d)
+
+            out_dict[s] = out
+
+        return out_dict
+
+    def plot_2d_mxr(self, subs, xcoord, ycoord, **kwargs):
+        """ Plot percentage bin2d output onto given axis. """
+        binned_seasonal = self.bin_seasonal(subs, xcoord, ycoord)
+        vlims, xlims, ylims = self.get_limits(subs, xcoord, ycoord)
+        
+        norm = Normalize(*vlims)
+        
+        fig, axs = plt.subplots(2, 2, dpi=250, figsize=(8,9), 
+                                sharey=True, sharex=True)
+
+        for season, ax in zip([1,2,3,4], axs.flatten()):
+            bin2d_inst = binned_seasonal[season]
+            ax.set_title(dcts.dict_season()[f'name_{season}'])
+
+            bci = bin2d_inst.binclassinstance
+            img = ax.imshow(bin2d_inst.vmean.T, 
+                            cmap = plt.cm.viridis, norm=norm,
+                            aspect='auto', origin='lower',
+                            extent=[bci.xbmin, bci.xbmax, bci.ybmin, bci.ybmax])
+            ax.set_xlim(xlims[0]*0.95, xlims[1]*1.05)
+            ax.set_ylim(ylims[0]*0.95, ylims[1]*1.05)
+
+            ax.set_xlabel(dcts.make_coord_label(xcoord))
+            ax.set_ylabel(dcts.make_coord_label(ycoord))
+            
+            if kwargs.get('note'): 
+                ax.text(**dcts.note_dict(ax, s=kwargs.get('note')))
+        
+        fig.subplots_adjust(right=0.9)
+        fig.tight_layout(pad=2.5)
+        
+        cbar = fig.colorbar(img, ax = axs.ravel().tolist(), aspect=30, pad=0.09, orientation='horizontal')
+        cbar.ax.set_xlabel(dcts.make_subs_label(subs))
+
+        plt.show()
+
+    def plot_mxr_diff(self, params_1, params_2, **kwargs): 
+        """ Plot difference between two plots. """
+        subs1, xcoord1, ycoord1 = params_1
+        subs2, xcoord2, ycoord2 = params_2
+
+        x_bin, y_bin = self.get_bins(*params_1)
+        bin_equi2d = bp.Bin_equi2d(np.nanmin(self.df[xcoord1.col_name]), 
+                                   np.nanmax(self.df[xcoord1.col_name]), 
+                                   x_bin, 
+                                   np.nanmin(self.df[ycoord1.col_name]), 
+                                   np.nanmax(self.df[ycoord1.col_name]),
+                                   y_bin)
+
+        binned_seasonal_1 = self.bin_seasonal(*params_1, bin_equi2d=bin_equi2d)
+        binned_seasonal_2 = self.bin_seasonal(*params_2, bin_equi2d=bin_equi2d)
+        
+        vlims, xlims, ylims = self.get_limits(*params_1)
+        cmap = plt.cm.PiYG
+
+        fig, axs = plt.subplots(2, 2, dpi=250, figsize=(8,9), 
+                                sharey=True, sharex=True)
+
+        for season, ax in zip([1,2,3,4], axs.flatten()):
+            ax.set_title(dcts.dict_season()[f'name_{season}'])
+            ax.set_facecolor('lightgrey')
+
+            # note simple substraction filters out everything where either is nan
+            vmean = binned_seasonal_1[season].vmean - binned_seasonal_2[season].vmean
+            vmax_abs = max(abs(np.nanmin(vmean)), abs(np.nanmax(vmean)))
+            norm = Normalize(-vmax_abs, vmax_abs) #TODO meh
+            
+            bci = binned_seasonal_1[season].binclassinstance
+
+            img = ax.imshow(vmean.T, cmap = cmap, norm=norm,
+                            aspect='auto', origin='lower', 
+                            extent=[bci.xbmin, bci.xbmax, bci.ybmin, bci.ybmax])
+            ax.set_xlim(xlims[0]*0.95, xlims[1]*1.05)
+            ax.set_ylim(ylims[0]*0.95, ylims[1]*1.05)
+
+            ax.set_xlabel(dcts.make_coord_label(xcoord1))
+            ax.set_ylabel(dcts.make_coord_label(ycoord1))
+            
+            if kwargs.get('note'): 
+                ax.text(**dcts.note_dict(ax, s=kwargs.get('note')))
+        
+        fig.subplots_adjust(right=0.9)
+        fig.tight_layout(pad=2.5)
+        
+        cbar = fig.colorbar(img, ax = axs.ravel().tolist(), aspect=30, 
+                            pad=0.09, orientation='horizontal')
+        cbar.ax.set_xlabel(
+            f'{dcts.make_subs_label(subs1)} {dcts.make_coord_label(xcoord1)} {dcts.make_coord_label(ycoord1)} \n vs.\n\
+{dcts.make_subs_label(subs2)} {dcts.make_coord_label(xcoord2)} {dcts.make_coord_label(ycoord2)}')
+        plt.show()
+
+
+    def plot_mixing_ratios(self, **kwargs):
+        """ Plot all possible permutations of subs, xcoord, ycoord. """
+        permutations = list(itertools.product(self.substances,
+                                              self.x_coordinates, 
+                                              self.y_coordinates))
+        for perm in permutations:
+            self.plot_2d_mxr(*perm, **kwargs)
+
+    def plot_differences(self, **kwargs):
+        """ Plot the mixing ratio difference between different substance cols and coordinates. """
+        permutations = list(itertools.product(self.substances,
+                                              self.x_coordinates, 
+                                              self.y_coordinates))
+
+        for params_1, params_2 in itertools.combinations(permutations, 2): 
+            if (params_1[0].short_name == params_2[0].short_name
+                and params_1[1].hcoord == params_2[1].hcoord
+                and params_1[2].vcoord == params_2[2].vcoord): 
+                # only compare same substance in same coordinate system
+                self.plot_mxr_diff(params_1, params_2, **kwargs)
+        
 
 #%% 2D plotting
 def get_right_data(c_obj, subs='n2o', c_pfx='INT2', detr=True):
-    substance = get_col_name(subs, c_obj.source, c_pfx)
+    substance = dcts.get_col_name(subs, c_obj.source, c_pfx)
     if substance is None: 
-        pfxs_avail = [pfx for pfx in c_obj.pfxs if subs in substance_list(pfx)]
+        pfxs_avail = [pfx for pfx in c_obj.pfxs if subs in dcts.substance_list(pfx)]
         if len(pfxs_avail)==0: print('No {subs} data available')
         else: c_pfx = input(f'No {c_pfx} data found for binning. Choose from {pfxs_avail}')
         if c_pfx not in c_obj.data.keys(): return
-        substance = get_col_name(subs, c_obj.source, c_pfx)
+        substance = dcts.get_col_name(subs, c_obj.source, c_pfx)
     
     if c_obj.source == 'Caribic': 
         try: data = c_obj.data[subs]
         except: data = c_obj.data[c_pfx]
     else: data = c_obj.df
-    data['season'] = make_season(data.index.month) # 1 = spring etc
+    data['season'] = tools.make_season(data.index.month) # 1 = spring etc
 
     if detr and not f'detr_{substance}' in data.columns:
         try: 
@@ -63,10 +282,57 @@ def seasonal_binning(data, substance, y_bin, y_coord, x_coord, x_bin, vlims):
 
     return out_dict, vmin, vmax
 
+
+def bin_seasonal(glob_obj, subs, subs_params={}, y_params={}, x_params={}, **kwargs) -> (dict, float, float):
+    """ Find substance, x, y, coordinates and calculate seasonal bins. """
+    
+    data = glob_obj.df
+    subs = dcts.get_subs(short_name=subs, source = glob_obj.source, **subs_params)
+
+    subs_col = subs.col_name
+    subs_label = dcts.make_subs_label(subs)
+    
+    if kwargs.get('detr') and 'detr_'+subs.col_name in data.columns:
+        subs_col = 'detr_' + subs_col
+        subs_label = f'{subs_label} detrended wrt. MLO 2005. '
+
+    y_coord = dcts.get_coord(**y_params)
+    y_label = dcts.make_coord_label(y_coord)
+    x_coord = dcts.get_coord(**x_params)
+    x_label = dcts.make_coord_label(x_coord)
+    
+    y_lim = np.nanmax(abs(data[y_params.col_name])) if not y_params.get('y_lim') else kwargs.get('y_lim')
+    y_bin = y_lim/10 if not y_params.get('y_bin') else y_params.get('y_bin')
+    x_lims = (-90, 90)
+    x_bin = 10 if not x_params.get('x_bin') else x_params.get('x_bin')
+
+    # seasonal_binning
+    vmin_list, vmax_list = [], []; out_dict = {}
+
+    # calculate binned output per season
+    for s in set(data['season'].tolist()):
+        df = data[data['season'] == s]
+        x = np.array(df[x_coord])
+        y = np.array(df[y_coord])
+        xbmin, xbmax, xbsize = np.nanmin(x), np.nanmax(x), x_bin
+        ybmin, ybmax, ybsize = np.nanmin(y), np.nanmax(y), y_bin
+
+        bin_equi2d = bp.Bin_equi2d(xbmin, xbmax, xbsize, ybmin, ybmax, ybsize)
+        out = bp.Simple_bin_2d(np.array(df[subs_col]), x, y, bin_equi2d)
+        out_dict[s] = out
+        vmin_list.append(np.nanmin(out.vmean))
+        vmax_list.append(np.nanmax(out.vmean))
+
+    if not vlims: vmin, vmax = (np.nanmin(vmin_list), np.nanmax(vmax_list))
+    else: vmin, vmax = vlims[0], vlims[1]
+
+    return out_dict, vmin, vmax
+
+
 def make_2d_plot(bin2d_inst, x_coord, y_coord, ax, season, note,
             x_lims, y_lims, vlims, x_label, y_label, percent=False):
     """ Plot percentage bin2d output onto given axis """
-    ax.set_title(dict_season()[f'name_{season}'])
+    ax.set_title(dcts.dict_season()[f'name_{season}'])
 
     if percent: 
         vmean_tot = np.nanmean(bin2d_inst.vmean)
@@ -100,7 +366,7 @@ def make_2d_MAD(bin2d_inst, x_coord, y_coord, ax, season, note,
         mu - mean (total)
         N - sample size
         """
-    ax.set_title(dict_season()[f'name_{season}'])
+    ax.set_title(dcts.dict_season()[f'name_{season}'])
 
     if percent: 
         vmean_tot = np.nanmean(bin2d_inst.vmean)
@@ -129,8 +395,9 @@ def make_2d_MAD(bin2d_inst, x_coord, y_coord, ax, season, note,
 
     return img
 
-def plot_2d_binned(c_obj, subs='n2o', v_pfx='GHG', y_params={}, x_params={}, 
-                   detr=True, vlims=None, note=None, percent=False, ylim_plt=None):
+def plot_2d_binned(glob_obj, subs, subs_params={}, y_params={}, x_params={}, **kwargs):
+                   # subs='n2o', subs_ID='GHG', y_params={}, x_params={}, subs_params={},
+                   # detr=True, vlims=None, note=None, percent=False, ylim_plt=None):
     """ Plot binned mxr on EqL vs. pot.T or 
     
     Creates plots of equivalent latitude versus potential temperature or height
@@ -149,46 +416,26 @@ def plot_2d_binned(c_obj, subs='n2o', v_pfx='GHG', y_params={}, x_params={},
         tp (str): tropopause definition. 'therm', 'dyn', 'z' or 'pvu'
         pvu (float): potential vorticity set as tropopause definition. 1.5, 2.0 or 3.5
     """
-    if (not all(i in x_params.keys() for i in  ['x_pfx', 'xcoord']) 
-    or not all(i in y_params.keys() for i in ['ycoord', 'y_pfx', 'tp_def'])): 
-        raise KeyError('Please supply all necessary parameters: x_pfx, xcoord / ycoord, tp_def, y_pfx, (pvu)')
-    
-    if not subs in c_obj.data.keys(): c_obj.create_substance_df(subs)
-    data, substance, v_pfx = get_right_data(c_obj, subs, v_pfx, detr)
-    y_coord, y_label, x_coord, x_label = coordinate_tools(**y_params, **x_params)
-    yextr = np.nanmax(abs(data[y_coord])) # most extreme value of y-coordinate
-    if ylim_plt is not None: yextr=ylim_plt
-
-    
-    if not 'y_bin' in y_params.keys(): y_bin = yextr / 10
-        # y_bins = {'z' : 0.5, 'pt' : 10, 'p' : 40}
-        # y_bin = y_bins[y_params['ycoord']]
-    else: y_bin = y_params['y_bin']
-    # y_lims = np.nanmin(data[y_coord])-y_bin, np.nanmax(data[y_coord])+y_bin
-
-    if not 'x_bin' in x_params.keys(): x_bin = 10
-    else: x_bin = x_params['x_bin']
-    x_lims = (-90, 90)
-
-    out_dict, vmin, vmax = seasonal_binning(data, substance, 
-                                            y_bin, y_coord, x_coord, x_bin, vlims)
+    out_dict, vmin, vmax = bin_seasonal(glob_obj, subs, subs_params, y_params, x_params, **kwargs)
 
     # Create plots for all seasons separately
-    f, axs = plt.subplots(2, 2, dpi=250, figsize=(9,7))
+    fig, axs = plt.subplots(2, 2, dpi=250, figsize=(9,7))
     for s, ax in zip(set(data['season'].tolist()), axs.flat): # flatten axs array
         out = out_dict[s] # take binned data for current season
         img = make_2d_plot(out, x_coord, y_coord, ax, s, note, 
-                           x_lims, (-yextr, yextr), (vmin, vmax), x_label, y_label, percent=percent)
-        
+                           x_lims, (-y_lim, y_lim), (vmin, vmax), x_label, y_label, percent=percent)
+
         # plot_variability(out, x_coord, y_coord, ax, s, note, 
         #               x_lims, y_lims, (vmin, vmax), x_label, y_label)
+        if kwargs.get('note'): 
+            ax.text(**dcts.note_dict(ax, s=note))
         
-    f.subplots_adjust(right=0.9)
+    fig.subplots_adjust(right=0.9)
     plt.tight_layout(pad=2.5)
-    cbar = f.colorbar(img, ax = axs.ravel().tolist(), aspect=30, pad=0.09)
-    xlabel = subs.upper() + ' ' + substance[substance.find('['):substance.find(']')+1]
-    if detr: xlabel = '$\Delta$ '+ xlabel + '\n wrt. 2005'
-    cbar.ax.set_xlabel(xlabel)
+
+    cbar = fig.colorbar(img, ax = axs.ravel().tolist(), aspect=30, pad=0.09)
+    cbar.ax.set_xlabel(subs_label)
+
     plt.show()
 
     return out_dict
@@ -199,7 +446,7 @@ def make_diff_plot(bin2d_inst1, bin2d_inst2,
                    mismatch_indic=False):
     """ Plot difference between plots """
 
-    ax.set_title(dict_season()[f'name_{season}'])
+    ax.set_title(dcts.dict_season()[f'name_{season}'])
 
     # NB simple substraction filters out everything where either is nan
     vmean = bin2d_inst1.vmean - bin2d_inst2.vmean
--- plot/gradients.py
+++ plot/gradients.py
@@ -3,7 +3,7 @@
 @Author: Sophie Bauchinger, IAU
 @Date: Fri Apr 28 14:13:28 2023
 
-Plotting of gradients - wants detrended data, sorted into atmos. layers
+Plot vertical profiles of Caribic data split into seasons. 
 
 """
 import numpy as np
@@ -15,31 +15,6 @@ import tools
 import dictionaries as dcts
 
 #%% Plotting Gradient by season
-# Fct definition in C_plot needed these:
-# select_var=['fl_ch4','fl_sf6', 'fl_n2o'] # flagged data
-# select_value=[0,0,0]
-# select_cf=['GT','GT', 'GT'] # operators
-
-# ptsmin (int): minimum number of pts for a bin to be considered #!!! implement
-
-# def plot_gradient_by_season(c_obj, subs, c_pfx = None, tp_def='therm', pvu = 3.5, errorbars=False,
-#                             detr=True, note=None, ycoord='pt', y_bin=None):
-    # """
-    # Plotting gradient by season using 1D binned data. Detrended data used by default
-    # (Inspired by C_plot.pl_gradient_by_season)
-
-    # Parameters:
-    #     c_obj (Caribic)
-    #     subs (str): substance e.g. 'sf6'
-    #     c_pfx (str): 'GHG', 'INT', 'INT2'
-    #     tp_def (str): tropopause definition
-    #     pvu (float): potential vorticity for dyn. tp definition. 1.5, 2.0 or 3.5
-    #     errorbars (bool)
-    #     y_bin (int): bin size for 1D binning (depends on coordinate)
-    #     detr (bool)
-    #     note (str): shown as text box on the plot
-    # """
-
 def plot_gradient_by_season(c_obj, subs_params = {}, y_params = {},
                             detr=True, note=None, errorbars=False):
     """
@@ -51,32 +26,26 @@ def plot_gradient_by_season(c_obj, subs_params = {}, y_params = {},
     y_params (dict):
         keys: vcoord, tp_def, ID, (pvu)
     """
-    if (not all(i in subs_params for i in ['short_name', 'ID'])
-                or not all(i in y_params.keys() for i in ['vcoord', 'ID', 'tp_def'])):
-        raise KeyError('Please supply all necessary parameters: subs: short_name, ID / y: vcoord, tp_def, ID, (pvu)')
-        
-    if not subs in c_obj.data.keys(): c_obj.create_substance_df(subs)
-    data = c_obj.data[subs]
+    substance = dcts.get_subs(**subs_params)
+    if not substance.short_name in c_obj.data.keys(): 
+        c_obj.create_substance_df(substance.short_name)
+    data = c_obj.data[substance.short_name]
 
-    # x-axis
-    substance = dcts.get_subs(substance=subs, ID=subs_params['ID']).col_name # subs, c_obj.source, x_params['ID'])
+    subs_col = substance.col_name # subs, c_obj.source, x_params['ID'])
     if detr: # detrended data for x-axis
-        substance = f'detr_{substance}'
-        if not substance in data.columns:
+        subs_col = f'detr_{subs_col}'
+        if not subs_col in data.columns:
             raise ValueError(f'Detrended data not available for {subs.upper()}')
 
     # y-axis
     y_coord = dcts.get_coordinates(**y_params)[0]
     y_label = dcts.make_coord_label(y_coord)
-    y_coord = y_coord.col_name #!!! bad code
-
-    # y_coord, y_label = coordinate_tools(**y_params)
     y_bins = {'z' : 0.5, 'pt' : 10, 'p' : 40}
     if not 'y_bin' in y_params.keys():
         y_bin = y_bins[y_params['vcoord']]
     else: y_bin = y_params['y_bin']
 
-    min_y, max_y = np.nanmin(data[y_coord].values), np.nanmax(data[y_coord].values)
+    min_y, max_y = np.nanmin(data[y_coord.col_name].values), np.nanmax(data[y_coord.col_name].values)
     nbins = (max_y - min_y) / y_bin
     y_array = min_y + np.arange(nbins) * y_bin + y_bin * 0.5
 
@@ -85,8 +54,8 @@ def plot_gradient_by_season(c_obj, subs_params = {}, y_params = {},
     fig, ax = plt.subplots(dpi=200)
     for s in set(data['season'].tolist()):
         df = data.loc[data['season'] == s]
-        y_values = df[y_coord].values # df[eq_lat_col].values #
-        x_values = df[substance].values
+        y_values = df[y_coord.col_name].values # df[eq_lat_col].values #
+        x_values = df[subs_col].values
 
         out_dict[f'bin1d_{s}'] = bp.Simple_bin_1d(x_values, y_values,
                                                   bp.Bin_equi1d(min_y, max_y, y_bin))
@@ -104,14 +73,14 @@ def plot_gradient_by_season(c_obj, subs_params = {}, y_params = {},
                          c=dcts.dict_season()[f'color_{s}'], elinewidth=0.5)
 
     plt.tick_params(direction='in', top=True, right=True)
-    if note: plt.annotate(note, xy=(0.025, 0.925), xycoords='axes fraction',
-                          bbox=dict(boxstyle="round", fc="w"))
+    if note: 
+        ax.text(**dcts.note_dict(ax, x=0, y=1.08, s=note))
 
     plt.ylim([min_y, max_y])
     plt.ylabel(y_label)
-    plt.xlabel(f'{substance}')
+    plt.xlabel(dcts.make_subs_label(substance, detr=detr))
     if detr: # remove the 'delta_' and replace with symbol
-        plt.xlabel(substance.split("_")[-1] + ' detrended wrt. 2005')
+        plt.xlabel(subs_col.split("_")[-1] + ' detrended wrt. 2005')
 
     plt.legend()
     plt.show()
@@ -119,7 +88,8 @@ def plot_gradient_by_season(c_obj, subs_params = {}, y_params = {},
 #%% Fct calls - gradients
 
 if __name__ == '__main__':
-    if False: caribic = True # BS to avoid error
+    from data import Caribic
+    caribic = Caribic() # BS to avoid error
 
     # yp1 = {'tp_def' : 'chem',
     #        'ID' : 'INT2',
@@ -139,9 +109,8 @@ if __name__ == '__main__':
            'pvu' : 3.5}
 
     for subs in ['sf6', 'n2o', 'co2', 'ch4']:
-        caribic.detrend(subs)
         for yp in [yp2, yp3, yp4]:
             subs_params = {'short_name': subs, 'ID' : 'GHG'}
-            y_params = yp
-            plot_gradient_by_season(caribic.sel_latitude(30, 90), subs_params, y_params, note='lat>30°N', detr=False)
-            plot_gradient_by_season(caribic.sel_latitude(30, 90), subs_params, y_params, note='lat>30°N', detr=True)
+            plot_gradient_by_season(caribic.sel_latitude(30, 90), subs_params, yp, note='lat>30°N', detr=False)
+            try: plot_gradient_by_season(caribic.sel_latitude(30, 90), subs_params, yp, note='lat>30°N', detr=True)
+            except ValueError: pass
