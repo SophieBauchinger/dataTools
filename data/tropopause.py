a6a341ea64ab141a6fee3f1e492fd7c9c8bf5ba2
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2025-03-10 09:07:55 +0100

    ozone tropopause calculations

--- data/tropopause.py
+++ data/tropopause.py
@@ -4,8 +4,11 @@
 @Author: Sophie Bauchinger, IAU
 @Date: Fri Dec 20 17:43:00 2024
 """
+import datetime as dt
 import numpy as np
+from pathlib import Path
 import pandas as pd
+from scipy.interpolate import interp1d
 
 from toolpac.conv.times import datetime_to_fractionalyear as dt_to_fy 
 from toolpac.outliers import outliers 
@@ -161,3 +164,189 @@ def tropo_strato_ratios(df_sorted, tps, **kwargs) -> tuple[pd.DataFrame]:
     ratio_df.loc['ratios'] = ratios  # set col
 
     return count_df, ratio_df
+
+# --- KIT Ozone tropopause calculation
+# ------------------------------------------------------------------------------
+
+def load_tp_hght_data(path: Path, sep=";", v_scal=1) -> dict:
+    """
+    Load rel. TP height data from csv file.
+
+    Parameters
+    ----------
+    path : str or Path
+        DESCRIPTION.
+    sep : str, optional
+        DESCRIPTION. The default is ";".
+    v_scal : int, optional
+        DESCRIPTION. The default is 1.
+
+    Returns
+    -------
+    dict
+        height relative to tropopause data.
+    """
+    with open(path, "r") as file_obj:
+        tp_data = file_obj.readlines()
+
+    for i, line in enumerate(tp_data):
+        tp_data[i] = line.strip().rsplit(sep)
+
+    tp_hght = np.array(tp_data[0][1:], dtype=float)
+    time = []
+    ozone = []
+
+    for line in tp_data[1:-1]:
+        time.append(int(line[0]))
+        ozone.append([float(o) * v_scal for o in line[1:]])
+
+    time = np.array(time)
+    ozone = np.array(ozone)
+
+    result = {}
+    result["tp_hght"] = tp_hght
+    result["montly_avg"] = {}
+    result["montly_avg"]["month"] = time
+    result["montly_avg"]["ozone"] = ozone
+
+    # convert time given as monthly mean to days of year
+    doy = time * 365 / 12 - (365 / 12 / 2)  # ignore leap years and so on...
+    time = np.array(list(range(1, 366)))
+    tmp_o3 = np.zeros([len(time), len(tp_hght)])
+    for i in range(len(tp_hght)):  # interpolate O3 for each TP height
+        f_ip = interp1d(
+            doy,
+            ozone[:, i],
+            kind="linear",
+            bounds_error=False,
+            fill_value="extrapolate",
+        )
+        tmp_o3[:, i] = f_ip(time)
+    ozone = tmp_o3
+
+    result["DOY_interp"] = {}
+    result["DOY_interp"]["DOY"] = time
+    result["DOY_interp"]["ozone"] = ozone
+
+    return result
+
+def calc_o3tp_relhght(
+    tpdata: dict,
+    v_ozone: float,
+    sel_month: int,
+    sel_day_of_month=False,
+    vmiss=9999,
+    v_lessthanmin=-9999,
+    v_ozone_min=60.0,
+    hreltp_min=-1.5,
+) -> float:
+    """
+    Find relative tropopause height for a given ozone value, v_ozone.
+    result is based on linear interpolation of rel. TP heights that correspond
+    to the ozone values closest to v_ozone.
+
+    Parameters
+    ----------
+    tpdata : dict
+        DESCRIPTION.
+    v_ozone : float
+        DESCRIPTION.
+    sel_month : int
+        DESCRIPTION.
+    sel_day : TYPE, optional
+        if specified, monthly tpdata is interpolated linearly to days of
+            year. The default is False.
+    vmiss : TYPE, optional
+        DESCRIPTION. The default is 9999.
+    v_ozone_min : TYPE, optional
+        DESCRIPTION. The default is 60.0.
+    hreltp_min : TYPE, optional
+        DESCRIPTION. The default is -1.5.
+
+    Returns
+    -------
+    float
+        height relative to tropopause or VMISS.
+    """
+    if v_ozone <= v_ozone_min:
+        return v_lessthanmin  # v_ozone too low, return vmiss
+
+    # first line: TP heights
+    tp_hght = tpdata["tp_hght"]
+
+    if sel_day_of_month:
+        sel_time = (
+            dt.datetime(2010, sel_month, sel_day_of_month) - dt.datetime(2010, 1, 1)  # doy from time-
+        ).days + 1  # delta object
+        time = tpdata["DOY_interp"]["DOY"]
+        ozone = tpdata["DOY_interp"]["ozone"]
+    else:
+        sel_time = sel_month
+        time = tpdata["montly_avg"]["month"]
+        ozone = tpdata["montly_avg"]["ozone"]
+
+    # find corresponding time index
+    ix_t = np.arange(len(time))[np.where(time == sel_time)]
+
+    # search corresponding ozone array for adjacent values
+    sel_ozone = ozone[ix_t]
+    sel_ozone = sel_ozone.reshape(len(sel_ozone[0]))  # flatten...
+    ix_close = np.array([np.argmin(np.abs(sel_ozone - v_ozone))], dtype=int)
+
+    if ix_close in (0, len(sel_ozone) - 1):
+        # no bracketing value! interpolation not possible,
+        return vmiss
+
+    # there is a bracketing value...
+    # find the next closest / = bracketing o3 value
+    o3_close = sel_ozone[ix_close]
+    sel_ozone[ix_close] = vmiss
+    ix_brack = np.array([np.argmin(np.abs(sel_ozone - v_ozone))], dtype=int)
+
+    # interpolate H_rel_TP based on rel. distance to given ozone value
+    rel_dist_o3 = np.abs(o3_close - v_ozone) / np.abs(o3_close - sel_ozone[ix_brack])
+    result = (tp_hght[ix_close] + (tp_hght[ix_brack] - tp_hght[ix_close]) * rel_dist_o3)[0]
+
+    if result <= hreltp_min:
+        result = v_lessthanmin  # h_rel_TP too low, return vmiss
+
+    return result
+
+def calc_HrelTP(df, o3_coord, dropna=False):
+    """ Calculate `o3tp_relheight` for the given ozone data.
+    
+    Parameters:
+        df (pd.DataFrame)
+        o3_coord (dcts.Substance | dcst.Coordinate)
+    """
+    tpdata = load_tp_hght_data(path = dcts.get_path() + r"data\O3_climatology_Hohenpeissenberg_for_H_rel_TP.csv")
+    v_ozone = df[o3_coord.col_name]
+    months = df.index.month
+    o3tp_relheight = [calc_o3tp_relhght(tpdata, v, m) for v,m in zip(v_ozone.values, months)]
+
+    hreltp = pd.Series(o3tp_relheight, 
+                       name = 'H_rel_TP', 
+                       index = df.index)
+    
+    # Replace value of vmiss with NaN (NB: setting vmiss=nan breaks the function)
+    hreltp.mask(hreltp == 9999., np.nan, inplace = True)
+    
+    tropo = 'tropo_H_rel_TP'
+    strato = 'strato_H_rel_TP'
+    
+    df_sorted = pd.DataFrame({strato: pd.Series(np.nan, dtype=object),
+                              tropo: pd.Series(np.nan, dtype=object)},
+                             index=df.index)
+    
+    df_sorted.loc[hreltp.lt(0), (strato, tropo)] = (False, True)
+    df_sorted.loc[hreltp.gt(0), (strato, tropo)] = (True, False)
+    
+    hreltp.mask(hreltp == -9999., np.nan, inplace = True)
+
+    return hreltp, df_sorted, v_ozone
+
+if __name__=='__main__':
+    from dataTools.data.Caribic import Caribic
+    caribic = Caribic()
+    [o3_subs] = caribic.get_substs(col_name = 'int_O3')
+    hreltp, df_sorted, v_ozone = calc_HrelTP(caribic.df, o3_subs)
