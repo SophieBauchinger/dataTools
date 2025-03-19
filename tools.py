f9736b1fb0b31bcb731b97e3b42deea9336c493b
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2025-03-19 10:16:45 +0100

    remove duplicates

--- tools.py
+++ tools.py
@@ -373,80 +373,20 @@ def minimise_tps(tps, vcoord=None) -> list:
     tps.sort(key=lambda x: x.tp_def)
     return tps
 
-# ---------------
-def get_shared_indices(self, tps=None, df=False):
-    """ Make reference for shared indices of chosen tropopause definitions. """
-    if 'df_sorted' not in self.data:
-        self.create_df_sorted()
-
-    data = self.df_sorted if not df else self.df
-    prefix = 'tropo_' if not df else ''
-
-    tps = self.tps if tps is None else tps
-
-    if self.source != 'MULTI':
-        tropo_cols = [prefix + tp.col_name for tp in tps if prefix + tp.col_name in data]
-        indices = data.dropna(subset=tropo_cols, how='any').index
-
-    else:
-        # Cannot do this without mashing together all the n2o / o3 tropopauses!
-        tps_non_chem = [tp for tp in tps if not tp.tp_def == 'chem']
-        tropo_cols_non_chem = [prefix + tp.col_name for tp in tps_non_chem if prefix + tp.col_name in data]
-        indices_non_chem = data.dropna(subset=tropo_cols_non_chem,
-                                        how='any').index
-        # Combine N2O tropopauses. (ignore Caribic O3 tropopause bc only one source)
-        tps_n2o = [tp for tp in tps if tp.crit == 'n2o']
-        tropo_cols_n2o = [prefix + tp.col_name for tp in tps_n2o if prefix + tp.col_name in data]
-        n2o_indices = data.dropna(subset=tropo_cols_n2o,
-                                    how='all').index
-
-        print('Getting shared indices using\nN2O measurements: {} and dropping O3 TPs: {}'.format(
-            [str(tp) + '\n' for tp in tps_non_chem],
-            [tp for tp in tps if tp not in tps_n2o + tps_non_chem]))
-
-        indices = indices_non_chem[[i in n2o_indices for i in indices_non_chem]]
-
-        # indices = [i for i in indices_non_chem if i in n2o_indices]
-
-    return indices
-# ---------------
-
 def get_shared_indices(data, variables):
     """ Make reference for shared indices of chosen variables / tps. 
     Parameters: 
         data (pd.DataFrame): data to get shared indices from
         variables (list[dcts.Substance|dcts.Coordinate]): 
             data needs to be available for these variables
-    
     """
     prefix = ''
     if not any(v.col_name in data.columns for v in variables):
         prefix = 'tropo_'
-
     tropo_cols = [prefix + v.col_name for v in variables if prefix + v.col_name in data]
     indices = data.dropna(subset=tropo_cols, how='any').index
-
-    # else: If source == 'MULTI', need to mash together all the n2o/o3 tropopauses... cba with this rn
-    #     # Cannot do this without mashing together all the n2o / o3 tropopauses!
-    #     tps_non_chem = [tp for tp in tps if not tp.tp_def == 'chem']
-    #     tropo_cols_non_chem = [prefix + tp.col_name for tp in tps_non_chem if prefix + tp.col_name in data]
-    #     indices_non_chem = data.dropna(subset=tropo_cols_non_chem,
-    #                                     how='any').index
-    #     # Combine N2O tropopauses. (ignore Caribic O3 tropopause bc only one source)
-    #     tps_n2o = [tp for tp in tps if tp.crit == 'n2o']
-    #     tropo_cols_n2o = [prefix + tp.col_name for tp in tps_n2o if prefix + tp.col_name in data]
-    #     n2o_indices = data.dropna(subset=tropo_cols_n2o,
-    #                                 how='all').index
-
-    #     print('Getting shared indices using\nN2O measurements: {} and dropping O3 TPs: {}'.format(
-    #         [str(tp) + '\n' for tp in tps_non_chem],
-    #         [tp for tp in tps if tp not in tps_n2o + tps_non_chem]))
-
-    #     indices = indices_non_chem[[i in n2o_indices for i in indices_non_chem]]
-
-    #     # indices = [i for i in indices_non_chem if i in n2o_indices]
-
     return indices
+
 # %% Data Handling
 def make_season(month) -> np.array:
     """ If given array of months, return integer representation of seasons
@@ -748,7 +688,7 @@ def skewness(x):
 
 
 # %% Plotting tools
-def add_zero_line(ax, axis='y'):
+def add_zero_line(ax):
     """ Highlight the gridline at 0 for the chosen axis on the given Axes object.
 
     Call when everything else has been plotted already, otherwise the limits will be messy.
