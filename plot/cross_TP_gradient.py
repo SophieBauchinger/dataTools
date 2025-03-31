b61e70639f7629b2439a7447dae8dbaff7f21149
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2025-03-31 19:42:49 +0200

    seasonal gradient comparison

--- plot/cross_TP_gradient.py
+++ plot/cross_TP_gradient.py
@@ -26,57 +26,87 @@ def seasonal_gradient_comparison(GlobalObject, subs, tps, **kwargs):
         GlobalObject (dataTools.data.GlobalData)
         subs (dcts.Substance|dcts.Coordinate)
         tps (list[dcts.Coordinate]]): TP-relative coordinates 
-    """
-    seas_tropo_avs = tp_tools.seasonal_tropospheric_average(GlobalObject, subs, tps)
-    
+    """   
     xbsize = kwargs.pop('xbsize', 0.5)
-    gradient_seas_norm_df = pd.DataFrame(columns = [tp.col_name for tp in tps])
+    
+    param_dict = {}
+    gradient_df = pd.DataFrame(columns = [tp.col_name for tp in tps])
     for tp in tps:
         bin_dict = bin_tools.seasonal_binning(
             GlobalObject.df, subs, tp, xbsize=xbsize, **kwargs)
         for s in bin_dict.keys():
             i_pos = next(x for x, val in enumerate(bin_dict[s].xintm) if val > 0) 
             continue
-        norm_gradients = dict()
+        norm_gradients, gradients = {}, {}
+        param_dict[tp.col_name] = {}
         for s in bin_dict.keys(): 
-            gradient = (bin_dict[s].vmean[i_pos] - bin_dict[s].vmean[i_pos-1]) / bin_dict[s].xbsize
-            norm_gradients[s] = gradient / seas_tropo_avs[tp.col_name][s]
+            i_pos_gradient = i_pos if not 'offset' in kwargs else i_pos+kwargs.get('offset')
+            pos_val = bin_dict[s].vmean[i_pos_gradient]
+            neg_val = bin_dict[s].vmean[i_pos_gradient-1]
+            gradient = (pos_val - neg_val) / bin_dict[s].xbsize
+            TP_average = np.mean(list(bin_dict[s].vbindata[i_pos]) + list(bin_dict[s].vbindata[i_pos-1]))
+            norm_gradients[s] = gradient / TP_average
+            gradients[s] = gradient
         
-        gradient_seas_norm_df[tp.col_name] = norm_gradients
+            param_dict[tp.col_name][s] = pd.Series(dict(
+                    neg_val = neg_val,
+                    TP_average = TP_average,
+                    pos_val = pos_val, 
+                    gradient = gradient,
+                    norm_gradient = gradient / TP_average
+                ))
+        gradient_df[tp.col_name] = norm_gradients if kwargs.get('norm', True) else gradients
         
-    # gradient_seas_norm_df.drop(columns = 'N2O_residual', inplace=True)
-    gradient_seas_norm_df.loc['Average'] = gradient_seas_norm_df.mean(axis=0)
+    gradient_df.loc['Average'] = gradient_df.mean(axis=0)
+    seasons = [dcts.dict_season()[f'name_{s}'].split()[0] for s in bin_dict.keys()]
 
-    seasons = [dcts.dict_season()[f'name_{s}'] for s in bin_dict.keys()]
+    param_df = pd.DataFrame.from_dict(
+        {(i,j): param_dict[i][j] 
+        for i in param_dict.keys() 
+        for j in param_dict[i].keys()},
+        orient='index')
 
     # Show the gradient fractions as heatmap
-    plt.figure(figsize=(6, 3), dpi=200)
-    ax = sns.heatmap(
-        gradient_seas_norm_df.T, 
+    if 'figax' in kwargs: 
+        fig, ax = kwargs.get('figax')
+    else: 
+        fig, ax = plt.subplots(figsize=(6, 3), dpi=500)
+    sns.heatmap(
+        gradient_df.T, 
         annot=True, cmap='YlGnBu', #cmr.get_sub_cmap('PRGn', 0.1, 0.9), # 'PRGn',# 'YlGnBu', 
         cbar_kws={'label': 'Gradient [fraction/km]'}, 
         yticklabels=[dcts.get_coord(c).label(filter_label=True) 
-                     for c in gradient_seas_norm_df.columns], 
+                     for c in gradient_df.columns], 
         
         xticklabels=seasons + ['Average'],
         
         # xticklabels=['Spring', 'Summer', 'Autumn', 'Winter', 'Average'],
-        fmt='.2f',
-        linewidths=1, linecolor='white',
+        fmt='.2f', ax=ax,
+        linewidths=1.5, linecolor='white',
         )
-    df = gradient_seas_norm_df.T
+    df = gradient_df.T
     # Highlight the "Average" row with a separate color (light gray)    
     av_values = df['Average']
     norm = Normalize(av_values.min(), av_values.max())
-    cmap = cmr.get_sub_cmap('binary', 0.2, 0.65)
+    cmap = cmr.get_sub_cmap('binary', 0.35, 0.7)
     facecolors = cmap([norm(v) for v in av_values])
     for i in range(len(df)):
         ax.add_patch(plt.Rectangle((len(df.columns)-1, i), 1, 1, # xy, width, height
                                    facecolor = facecolors[i],# 'lightgrey', 
                                    lw=2, edgecolor='white'))
-    plt.show()
-    return gradient_seas_norm_df.T
+    if 'figax' in kwargs:
+        return fig, ax
+    else:
+        plt.show()
+    return gradient_df.T, param_df
 
+def format_gradient_params_LATEX(gradient_params): 
+    """ Returns very specific formatting of the gradient parameter dataframe to copy into .tex file """
+    seasons = gradient_params.reset_index()['level_1'].apply(lambda x: '& ' + dcts.dict_season()[f'name_{x}'].split(' ')[0])
+    param_df = gradient_params.reset_index().drop(columns = ['level_0'])
+    param_df['level_1']  = seasons
+    param_df.set_index('level_1', inplace=True)
+    print(param_df.to_latex(header=False, float_format="%.2f"))
 
 def plot_1d_gradient(ax, s, bin_obj,
                      bin_attr: str = 'vmean', 
