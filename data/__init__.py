9af1a6cae291b69d918d64e4d6cdad08fa51fb29
SophieBauchinger <bauchinger@iau.uni-frankfurt.de>
2026-01-26 09:41:09 +0100

    remove unimportant upper level imports

--- data/__init__.py
+++ data/__init__.py
@@ -1,11 +1,12 @@
 from ._caribic import Caribic
-from ._campaigns import CampaignData, CampaignSQLData, DataCollection
-from ._model import Era5ModelData, EMAC, Mozart
+from ._campaigns import CampaignData, DataCollection # CampaignSQLData
+# from ._model import Era5ModelData, EMAC, Mozart
 from ._local import MaunaLoa, MaceHead
 
-from .tropopause import n2o_baseline_filter
-from .BinnedData import binning, seasonal_binning, monthly_binning
+# from .tropopause import n2o_baseline_filter
+# from .BinnedData import binning, seasonal_binning, monthly_binning
 
 if __name__=="__main__":
-    Caribic, CampaignData, CampaignSQLData, DataCollection, Era5ModelData, EMAC, Mozart, MaunaLoa, MaceHead
-    n2o_baseline_filter,binning, seasonal_binning, monthly_binning
+    Caribic, CampaignData, DataCollection, MaunaLoa, MaceHead
+    # Era5ModelData, EMAC, Mozart, CampaignSQLData, 
+    # n2o_baseline_filter,binning, seasonal_binning, monthly_binning
