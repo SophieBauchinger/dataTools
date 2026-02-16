43fe0c3f79030dc1cbb69655eb3f26155bca7027
SophieBauchinger <124771629+SophieBauchinger@users.noreply.github.com>
2026-02-16 15:18:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setuptools.setup(
     author="SophieBauchinger",
     author_email="bauchinger@iau.uni-frankfurt.de",
     description="Useful tools for analysing atomospheric measurement data",
-    url="https://github.com/AtmosphericAngels/toolpac.git",
+    url="https://github.com/SophieBauchinger/dataTools.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>3.6',
     install_requires=[
         'pandas',
         'numpy',
